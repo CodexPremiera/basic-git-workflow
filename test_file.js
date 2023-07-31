8e84a6b16184b080a2deb53d9b939d7049797d5b
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-07-31 14:13:41 +0800

    First Commit | Stage the original version of the file and add some changes.

---
+++ test_file.js
@@ -0,0 +1,6 @@
+/* ==================== ORIGINAL EDITION ==================== */
+console.log("Hello World! Welcome to JavaScript!");
+console.log();
+
+// S1: Initialize local directory as git repository
+// S2: Stage (commit) the file to the repository
