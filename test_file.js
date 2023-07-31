e1c0f7702dbc091fce3a64e4ef5d8b67dfe1f651
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-07-31 14:21:09 +0800

    2nd Commit | Push the file to the remote repository

--- test_file.js
+++ test_file.js
@@ -4,3 +4,6 @@ console.log();
 
 // S1: Initialize local directory as git repository
 // S2: Stage (commit) the file to the repository
+
+/* ==================== FIRST EDITION ==================== */
+// S3: Push the file to thee remote repository
